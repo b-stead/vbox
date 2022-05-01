ed190218c535b9c50437067117e53ca5c88117d5
Brendon stead <97539241+b-stead@users.noreply.github.com>
2022-05-01 18:14:07 +0100

    Update SR.py
    tidying up

--- SR.py
+++ SR.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 import numpy as np
 from butterworth import df
+import os
 #df = pd.read_csv("DA500.csv")
-
 
 df2 = df[['velocity', 'time_of_day', 'Distance','Vfilt2', 'SplitD']].copy()
 x=df2.Distance
@@ -24,10 +24,7 @@ min_height = yneg[minima[0]] #list of the mirrored minima heights
 #minima_height = minima[1]['peak_heights']
 min_pos = x[minima[0]] #list of the minima positions
 
-
-
 #Plotting minima
-"""
 fig = plt.figure()
 ax = fig.subplots()
 ax.scatter(x,y)
@@ -36,7 +33,6 @@ ax.scatter(min_pos, min_height*-1, color = 'gold', s = 15, marker = 'X', label =
 ax.legend()
 ax.grid()
 plt.show()
-"""
 
 #concat min_pos& min_height into dataframe
 #add stroke position back onto df values
@@ -67,7 +63,6 @@ df3['boolean'] = df3['count'].astype('bool')
 SR = df3.Str_Rate
 SRM3 = df3.SRMean3
 
-
 #remove points out of range
 x2=x[SR>0]
 SRa = SR[SR>0]
@@ -75,13 +70,10 @@ SRa = SR[SR>0]
 x3=x[SRM3>0]
 SR3 = SRM3[SRM3>0]
 
-"""
 plt.plot(x2,SRa, label='SR')
 plt.plot(x3,SR3, label='SR3')
 plt.legend()
 plt.show()
-
-
 
 fig, ax1 = plt.subplots()
 
@@ -99,12 +91,11 @@ ax2.tick_params(axis='y', labelcolor=color)
 
 fig.tight_layout()
 plt.show()
-"""
-df4=df3[['velocity', 'Distance', 'Vfilt2', 'SRMean3', 'SplitD']]
+
 #move df to csv
-import os  
+df4=df3[['velocity', 'Distance', 'Vfilt2', 'SRMean3', 'SplitD']]
 os.makedirs('folder/subfolder', exist_ok=True)  
 df4.to_csv('folder/subfolder/df4.csv')
-#print(df4)
+
 
 
